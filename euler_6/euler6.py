8fce9842f198ede66e3013bc7c2c50400aa35872
tomazcuber <tomaz_br_23@hotmail.com>
2019-08-23 20:15:34 -0300

    Adiciona docstring nas funções euler_6, soma_dos_naturais e soma_dos_quadrados.

--- euler_6/euler6.py
+++ euler_6/euler6.py
@@ -1,14 +1,16 @@
 def euler_6(n=100):
-        """Uma breve explicação do que a função faz"""
+        """Resolve a diferença entre o quadrado da soma e a soma dos quadrados dos n primeiros números naturais."""
         soma_quadrados = soma_dos_quadrados(n)
         quadrado_soma = soma_dos_naturais(n) ** 2
 
         return soma_quadrados, quadrado_soma, quadrado_soma - soma_quadrados
 
 def soma_dos_quadrados(n):
+    """Retorna a soma dos quadrados dos n primeiros números naturais."""
     return ((n+1)*n*(2*n+1)) // 6
 
 def soma_dos_naturais(n):
+    """Retorna soma dos n primeiros números naturais."""
     if n % 2 == 0:
         return (n//2)*(1 + n)
     else:
