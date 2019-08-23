ead85c31c4a84bff45ff8221d7f9bfaf6898e611
tomazcuber <tomaz_br_23@hotmail.com>
2019-08-23 18:54:49 -0300

    Refatora a solução do exercício Euler 6, agora recebe a quantidade de números a serem somados como input

--- euler_6/euler6.py
+++ euler_6/euler6.py
@@ -14,7 +14,11 @@ def soma_dos_naturais(n):
     else:
         return ((n//2) + 1) * n
 
-print("A soma dos quadrados dos primeiros {} números naturais é: {}".format(100, euler_6()[0]))
-print("O quadrado da soma dos {} primeiros números naturais é {}".format(100,euler_6()[1]))
-#print("A soma dos {} primeiros números naturais é {}".format(99,soma_dos_naturais(99)))
-print("{} - {} = {}".format(euler_6()[1],euler_6()[0],euler_6()[2]))
+if __name__ == "__main__":
+    print("Digite a quantidade de númeos naturais da entrada: ");
+    n = int(input())
+
+    print("a soma dos quadrados dos primeiros {} números naturais é: {}".format(100, euler_6(n)[0]))
+    print("o quadrado da soma dos {} primeiros números naturais é {}".format(100,euler_6(n)[1]))
+    #print("a soma dos {} primeiros números naturais é {}".format(99,soma_dos_naturais(99)))
+    print("{} - {} = {}".format(euler_6(n)[1],euler_6(n)[0],euler_6(n)[2]))
